2f3bbbd86620d0476434e1d98496679cafea1c97
Cami-Marti <104392891+Cami-Marti@users.noreply.github.com>
2023-04-13 13:51:21 +0200

    UserBanner creation

---
+++ src/components/UserBanner/index.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+import './style.scss';
+
+const UserBanner = ({data}) => {
+
+return (
+  <div className="flex wrap" key={data[1].id}>
+    <div>
+        <img className="rounded-full" src={data[1].url_img} alt="photo de profil"/>
+    </div>
+    <div>
+        <p className="user-nickname">{data[1].nickname}</p>
+        <p className="user-email">{data[1].email}</p>
+        <p className="position absolute">{data[1].role}</p>
+    </div>
+  </div>
+)};
+UserBanner.propTypes = {
+  url_img: PropTypes.string.isRequired,
+  nickname: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+};
+
+export default UserBanner;
