6b9984de88ff01aab221bdd4e6318ea8ba7d7f7e
Cami-Marti <104392891+Cami-Marti@users.noreply.github.com>
2023-04-14 17:00:43 +0200

    add route on Edit component

--- src/components/Edit/index.jsx
+++ src/components/Edit/index.jsx
@@ -1,9 +1,11 @@
-
+import { Link, Router,Route } from 'react-router-dom';
 import UserEdit from './UserEdit';
 import SuitEdit from './SuitEdit';
 import InstrumentEdit from './InstrumentEdit';
 
 import users from '../../../data/users.json';
+
+
 
 const Edit = () => {
 
@@ -13,16 +15,21 @@ const Edit = () => {
   }
 
     return (
-      <div>
-        <UserEdit data={users}
+      <Router>
+        <Route path="/member/user/:id/edit" element={SuitEdit }/>
+        <Route path="/member/user/:id/add" element={SuitEdit }/>
+
+        <UserEdit
         OnSubmitFormUser={handleAdd} 
         />
+        <Route path="/member/suits/:id/edit" element={SuitEdit }/>
         <SuitEdit 
         OnSubmitFormSuit={handleAdd} />
+        <Route path="/member/instruments/:id/edit" element={InstrumentEdit} />
         <InstrumentEdit 
         OnSubmitFormInstrument={handleAdd}/>
         
-      </div>
+        </Router>
     )
   }
 export default Edit;
