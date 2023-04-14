36c837835a75547ae5c5d7716fa368bca9a90735
Cami-Marti <104392891+Cami-Marti@users.noreply.github.com>
2023-04-14 14:36:24 +0200

    Form structure

--- src/components/Edit/UserEdit/index.jsx
+++ src/components/Edit/UserEdit/index.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 
 const UserEdit= ({data}) => {
+  console.log(data[1].top_size)
 return(
  <form
+
  autoComplete="off"
  className="login-form-element">
     <div className='flex wrap'>
@@ -10,22 +12,27 @@ return(
          <input 
          type='text'
          className='form-input'
+         placeholder={data[1].lastname}
+         
          />
          <label>Prénom</label>
          <input
          type='text'
          className='form-input'
+         value={data[1].firstname}
         />
         <label>Pseudo</label>
         <input
          type='text'
          className='form-input'
+         placeholder={data[1].nickname}
          />
     </div>
     <label>Email</label>
         <input
         type='email'
         className='form-input'
+        placeholder={data[1].email}
        />
        <button
         type='submit'
@@ -37,41 +44,44 @@ return(
       <input
         type='date'
         className='form-input'
+        placeholder={data[1].birthdate}
       />
       <label>Téléphone</label>
       <input
         type='tel'
         className='form-input'
+        placeholder={data[1].phone}
       />
     </div>  
     <div>
       <label >Genre</label>
-    <select name="genre" >
+    <select name="genre"
+    placeholder={data[1].gender} >
         <option value=""></option>
-        <option value="femme">FEMME</option>
-        <option value="homme">HOMME</option>
+        <option value="femme">F</option>
+        <option value="homme">M</option>
     </select>
     <label >Taille Haut</label>
     <select name="Taille-haut" >
-        <option value=""></option>
+        <option value={data[1].top_size}></option>
         <option value="xs">XS</option>
         <option value="s">S</option>
         <option value="m">M</option>
         <option value="l">L</option>
         <option value="xl">XL</option>
         <option value="xxl">XXL</option>
-        <option value="xxl">XXL</option>
+        <option value="xxxl">XXXL</option>
     </select>
     <label >Taille Bas</label>
     <select name="Taille-bas" >
-        <option value=""></option>
+        <option value={data[1].bottom_size}></option>
         <option value="xs">XS</option>
         <option value="s">S</option>
         <option value="m">M</option>
         <option value="l">L</option>
         <option value="xl">XL</option>
         <option value="xxl">XXL</option>
-        <option value="xxl">XXL</option>
+        <option value="xxxl">XXXL</option>
     </select>
       
     </div>
@@ -101,7 +111,7 @@ return(
     </div>  
  
 {/* PART Members and Admin */}
-{data[0].role === ("member" || "admin") && (
+{data[1].role === ("member" || "admin") && (
     <fieldset>
         <legend>Adhésion</legend>
      <div>
@@ -113,14 +123,13 @@ return(
     </fieldset>
 )}
 {/* Part Admin */}
-{data[0].role === ("admin") && (
+{data[1].role === ("admin") && (
   <div>
     <label >Choisir un rôle</label>
     <select name="role" id="role">
         <option value="Adhérent">Adhérent</option>
         <option value="Bureau">Bureau</option>
         <option value="Admin">Admin</option>
-        <option value="Ajouter">Ajouter un rôle</option>
     </select>
     </div>
 )}
