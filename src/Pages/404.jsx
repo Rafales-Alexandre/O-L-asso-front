0fe987be8e52fd7cfc956071865e5395eb2dd454
Stanislas-GOTCH <sgoclock@gmail.com>
2023-04-26 14:46:04 +0200

    pages clean style

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -4,18 +4,18 @@ export const 404 = () => {
   return (
   <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
     <div class="mx-auto max-w-screen-sm text-center">
-      <h1 class="text-primary-600 dark:text-primary-500 mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
+      <h1 class="text-primary mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
         404
       </h1>
-      <p class="mb-4 text-3xl font-bold tracking-tight text-primary dark:text-white md:text-4xl">
+      <p class="mb-4 text-3xl font-bold tracking-tight text-primary md:text-4xl">
         Oops!
       </p>
-      <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+      <p class="mb-4 text-lg font-light text-accent">
         On dirait que cette page a quitté la scène sans jouer de morceau...{" "}
       </p>
       <a
         href="/"
-        class="bg-primary-600 hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-900 my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+        class="bg-primary hover:bg-success focus:ring-primary my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
       >
         Retour
       </a>
