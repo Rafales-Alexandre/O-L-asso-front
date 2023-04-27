8766d088d346f9c4a9013a03a279ae714f5e13fa
Stanislas-GOTCH <sgoclock@gmail.com>
2023-04-27 09:41:57 +0200

    apollo new url

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,17 +1,17 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: 'http://anaelleighil-server.eddi.cloud/graphql',
+  uri: "https://projet-01-o-lasso-back-production.up.railway.app/graphql",
 });
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token || '',
+      authorization: token || "",
     },
   };
 });
