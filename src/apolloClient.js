377226e858cce9affd5a09467aa16702f45acf06
Alexandre Rafales <92779990+Rafales-Alexandre@users.noreply.github.com>
2023-07-15 23:14:07 +0200

    Update apolloClient.js

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -7,9 +7,7 @@ const httpLink = createHttpLink({
   fetch: fetch,
 });
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem("token");
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
